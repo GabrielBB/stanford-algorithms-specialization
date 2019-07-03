a76c26eed4e0fa3c36e49db2d3ab98596b58e2e8
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-07-02 20:21:01 -0400

    Initial Commit

---
+++ src/main/java/com/github/gabrielbb/stanford/algorithms/App.java
@@ -0,0 +1,13 @@
+package com.github.gabrielbb.stanford.algorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/github/gabrielbb/stanford/algorithms/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.gabrielbb.stanford.algorithms;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

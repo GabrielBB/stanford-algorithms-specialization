32132ff3001256580bc9b8a2a0dac860df432578
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-07-02 20:56:38 -0400

    Initial commit for GradeSchoolMultiplication. Week 1, Course 1

---
+++ src/main/java/com/github/gabrielbb/stanford/algorithms/course1/GradeSchoolMultiplication.java
@@ -0,0 +1,15 @@
+package com.github.gabrielbb.stanford.algorithms.course1;
+
+public class GradeSchoolMultiplication {
+
+    public long solve(long a, long b) {
+
+        long ad = a % 10;
+        long bd = b % 10;
+
+        long sum = ad + bd;
+
+        
+        return 0;
+    }
+}
